f41607f2b645ac77df3e455f0b110e4928c98253
pixcelo <bodomu@gmail.com>
2022-08-14 15:20:37 +0900

    preload

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+        preload: path.join(__dirname, 'preload.js'),
+    },
   });
 
-  win.loadFile('index.html')
+  win.loadFile('index.html');
+
+  // open devTools
+  win.webContents.openDevTools();
 };
 
 app.whenReady().then(() => {
---
+++ preload.js
@@ -0,0 +1,8 @@
+const { contextBridge } = require('electron');
+
+// publish as global variable
+contextBridge.exposeInMainWorld('versions', {
+    node: () => process.versions.node,
+    chrome: () => process.versions.chrome,
+    electron: () => process.versions.electron,
+});
