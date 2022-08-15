6d576d8ad0fad39f05c2ff93791b8282be8e1d20
pixcelo <bodomu@gmail.com>
2022-08-16 03:39:40 +0900

    renderer.js

--- renderer.js
+++ renderer.js
@@ -1,28 +1,30 @@
-const information = document.getElementById('info');
-information.innerText = `This app is using Chrome (v${versions.chrome()}), Node.js (v${versions.node()}), and Electron (v${versions.electron()})`;
-
-// const func = async () => {
-//     const response = await window.versions.ping()
-//     console.log(response);
-// }
-
-// func();
-
-// Notification of Renderer process
-// const NOTIFICATION_TITLE = 'Title';
-// const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.';
-// const CLICK_MESSAGE = 'Notification clicked!';
-
-// new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY })
-//   .onclick = () => document.getElementById("output").innerText = CLICK_MESSAGE;
-
-// Dialog
-const btn = document.getElementById('btn');
-// const text = document.getElementById('text');
-// btn.onclick = async () => {
-//     text.textContent = await window.apis.openDialog();
-// };
-btn.onclick = async () => {
-    await window.apis.mkDir();
+// Login
+const btnLogin = document.getElementById('btnLogin');
+btnLogin.addEventListener('click', async () => {
     await window.apis.openBrowser();
-};
+});
+
+// Browse
+const btnLoad = document.getElementById('btnLoad');
+const uploadFile = document.getElementById('uploadFile');
+btnLoad.addEventListener('click', async () => {
+    uploadFile.value = await window.apis.openDialog();
+});
+
+// Run
+const btnRun = document.getElementById('btnRun');
+btnRun.addEventListener('click', async () => {
+    window.apis.loadCsvFile(uploadFile.value);
+});
+
+// Exit
+const btnExit = document.getElementById('btnExit');
+btnExit.addEventListener('click', async () => {
+    await window.apis.closeApp();
+});
+
+// Download test
+const btnTest = document.getElementById('btnTest');
+btnTest.addEventListener('click', async () => {
+    await window.apis.downloadFile();
+});
